1ae17472a06bff8821ac59108c6520d7a022ffd1
Wesley-Sobrinho <sobrinhoarquitetura@hotmail.com>
2022-12-09 20:13:53 -0300

    Desafio concluído !

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,21 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
-    {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+    public class Iphone : Smartphone
+{ /// <summary>
+/// 
+/// </summary>
+/// <param name="numero"></param>
+/// <param name="modelo"></param>
+/// <param name="imei"></param>
+/// <param name="memoria"></param>
+        public Iphone(string numero, string modelo, int imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo '{nomeApp}' no Iphone!");
+        }
     }
 }
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,22 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
-    {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+    
+    public class Nokia : Smartphone
+
+    { /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="numero"></param>
+    /// <param name="modelo"></param>
+    /// <param name="imei"></param>
+    /// <param name="memoria"></param>
+        public Nokia(string numero, string modelo, int imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo '{nomeApp}' no Nokia!");
+        }
     }
 }
